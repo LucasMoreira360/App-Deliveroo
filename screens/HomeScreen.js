2796b93c6707189f055771b6920322d06e3399d7
Lucas Moreira <lucas_10_moreira@hotmail.com>
2023-03-09 23:56:19 -0300

    primeira tela semi-finalizada

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,21 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
   UserIcon,
   ChevronDownIcon,
-  SearchIcon,
-  AdjustmentsIcon,
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassIcon,
 } from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -19,32 +28,73 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-      <Text >
-        {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-          <Image
-            source={{
-              uri: "https://links.papareact.com/wru",
-            }}
-            className="h-7 w-7 bg-gray-300  mt-8 rounded-full"
-          />
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300  mt-8 rounded-full"
+        />
 
-          <View className="w-full h-full rounded-md object-cover left-40" >
-            <Text
-              className="font-bold text-gray-400 text-xs
+        <View>
+          <Text
+            className="font-bold text-gray-400 text-xs
             mt-7"
-            >
-              Deliver Now!
-            </Text>
-            <Text className="font-bold text-xl ">
-              Current Location
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
-
+          >
+            Deliver Now!
+          </Text>
+          <Text className="font-bold text-xl ">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <View className="left-[110px] mt-10">
           <UserIcon size={35} color="#00CCBB" />
         </View>
-      </Text>
+      </View>
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4  ">
+        <View className="h-[20] w-80 flex-row  space-x-2 bg-gray-200 p-1 ">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+
+        <AdjustmentsVerticalIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Body*/}
+      <ScrollView
+        className="bg-gray-100 "
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        {/*Categories*/}
+        <Categories />
+
+        {/*Featured Rows*/}
+        <FeaturedRow
+          id="123"
+          title="Featured"
+          description="Paid placements from our partners"
+        />
+        {/*Tasty Discounts*/}
+        <FeaturedRow
+          id="1234"
+          title="Tasty Discounts"
+          description="Everyone's been enjoying these juicy discounts!"
+        />
+        {/*Offers near you*/}
+        <FeaturedRow
+          id="12345"
+          title="Offers near you!"
+          description="Why not support your local restaurant tonight!"
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
